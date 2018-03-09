bf7a7828f54cdd3164cf3080d3b4384a4fb16254
astobuze237 <astobuze237@ed.amdsb.ca>
2018-03-09 13:03:12 -0500

    Add project files.

---
+++ SecondSummative/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace SecondSummative
+{
+    partial class background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.collarLabel = new System.Windows.Forms.Label();
+            this.treatLabel = new System.Windows.Forms.Label();
+            this.toyLabel = new System.Windows.Forms.Label();
+            this.collarInput = new System.Windows.Forms.TextBox();
+            this.toysInput = new System.Windows.Forms.TextBox();
+            this.treatsInput = new System.Windows.Forms.TextBox();
+            this.itemsButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.dividerLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.neworderButton = new System.Windows.Forms.Button();
+            this.stLabel = new System.Windows.Forms.Label();
+            this.txLabel = new System.Windows.Forms.Label();
+            this.ttlabel = new System.Windows.Forms.Label();
+            this.receiptbackgroundLabel = new System.Windows.Forms.Label();
+            this.chLabel = new System.Windows.Forms.Label();
+            this.coverLabel = new System.Windows.Forms.Label();
+            this.warningLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe Print", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(303, 21);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(426, 75);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Astoria\'s Pet Shop";
+            // 
+            // collarLabel
+            // 
+            this.collarLabel.BackColor = System.Drawing.Color.Transparent;
+            this.collarLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.collarLabel.Location = new System.Drawing.Point(12, 176);
+            this.collarLabel.Name = "collarLabel";
+            this.collarLabel.Size = new System.Drawing.Size(246, 55);
+            this.collarLabel.TabIndex = 1;
+            this.collarLabel.Text = "Number of Collars";
+            // 
+            // treatLabel
+            // 
+            this.treatLabel.BackColor = System.Drawing.Color.Transparent;
+            this.treatLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treatLabel.Location = new System.Drawing.Point(12, 231);
+            this.treatLabel.Name = "treatLabel";
+            this.treatLabel.Size = new System.Drawing.Size(246, 55);
+            this.treatLabel.TabIndex = 2;
+            this.treatLabel.Text = "Number of Treats";
+            // 
+            // toyLabel
+            // 
+            this.toyLabel.BackColor = System.Drawing.Color.Transparent;
+            this.toyLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toyLabel.Location = new System.Drawing.Point(12, 286);
+            this.toyLabel.Name = "toyLabel";
+            this.toyLabel.Size = new System.Drawing.Size(246, 55);
+            this.toyLabel.TabIndex = 3;
+            this.toyLabel.Text = "Number of Toys";
+            // 
+            // collarInput
+            // 
+            this.collarInput.Location = new System.Drawing.Point(273, 181);
+            this.collarInput.Name = "collarInput";
+            this.collarInput.Size = new System.Drawing.Size(100, 31);
+            this.collarInput.TabIndex = 4;
+            // 
+            // toysInput
+            // 
+            this.toysInput.Location = new System.Drawing.Point(273, 291);
+            this.toysInput.Name = "toysInput";
+            this.toysInput.Size = new System.Drawing.Size(100, 31);
+            this.toysInput.TabIndex = 5;
+            // 
+            // treatsInput
+            // 
+            this.treatsInput.Location = new System.Drawing.Point(273, 236);
+            this.treatsInput.Name = "treatsInput";
+            this.treatsInput.Size = new System.Drawing.Size(100, 31);
+            this.treatsInput.TabIndex = 6;
+            // 
+            // itemsButton
+            // 
+            this.itemsButton.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemsButton.Location = new System.Drawing.Point(62, 335);
+            this.itemsButton.Name = "itemsButton";
+            this.itemsButton.Size = new System.Drawing.Size(278, 50);
+            this.itemsButton.TabIndex = 7;
+            this.itemsButton.Text = "Calculate Totals";
+            this.itemsButton.UseVisualStyleBackColor = true;
+            this.itemsButton.UseWaitCursor = true;
+            this.itemsButton.Click += new System.EventHandler(this.itemsButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtotalLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(12, 398);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(246, 55);
+            this.subtotalLabel.TabIndex = 8;
+            this.subtotalLabel.Text = "Sub Total";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.BackColor = System.Drawing.Color.Transparent;
+            this.taxLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(12, 443);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(246, 55);
+            this.taxLabel.TabIndex = 9;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.totalLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(12, 486);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(246, 55);
+            this.totalLabel.TabIndex = 10;
+            this.totalLabel.Text = "Total";
+            // 
+            // dividerLabel
+            // 
+            this.dividerLabel.BackColor = System.Drawing.Color.Black;
+            this.dividerLabel.Location = new System.Drawing.Point(12, 541);
+            this.dividerLabel.Name = "dividerLabel";
+            this.dividerLabel.Size = new System.Drawing.Size(372, 22);
+            this.dividerLabel.TabIndex = 11;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tenderedLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(11, 573);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(246, 55);
+            this.tenderedLabel.TabIndex = 12;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(273, 578);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(100, 31);
+            this.tenderedInput.TabIndex = 13;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(62, 631);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(278, 50);
+            this.changeButton.TabIndex = 14;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.UseWaitCursor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.changeLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(12, 696);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(246, 55);
+            this.changeLabel.TabIndex = 15;
+            this.changeLabel.Text = "Change";
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(62, 754);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(278, 50);
+            this.receiptButton.TabIndex = 16;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.UseWaitCursor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // neworderButton
+            // 
+            this.neworderButton.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.neworderButton.Location = new System.Drawing.Point(603, 754);
+            this.neworderButton.Name = "neworderButton";
+            this.neworderButton.Size = new System.Drawing.Size(278, 50);
+            this.neworderButton.TabIndex = 31;
+            this.neworderButton.Text = "New Order";
+            this.neworderButton.UseVisualStyleBackColor = true;
+            this.neworderButton.UseWaitCursor = true;
+            this.neworderButton.Click += new System.EventHandler(this.neworderButton_Click);
+            // 
+            // stLabel
+            // 
+            this.stLabel.BackColor = System.Drawing.Color.Transparent;
+            this.stLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stLabel.Location = new System.Drawing.Point(267, 398);
+            this.stLabel.Name = "stLabel";
+            this.stLabel.Size = new System.Drawing.Size(180, 45);
+            this.stLabel.TabIndex = 40;
+            // 
+            // txLabel
+            // 
+            this.txLabel.BackColor = System.Drawing.Color.Transparent;
+            this.txLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txLabel.Location = new System.Drawing.Point(267, 443);
+            this.txLabel.Name = "txLabel";
+            this.txLabel.Size = new System.Drawing.Size(180, 45);
+            this.txLabel.TabIndex = 41;
+            // 
+            // ttlabel
+            // 
+            this.ttlabel.BackColor = System.Drawing.Color.Transparent;
+            this.ttlabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ttlabel.Location = new System.Drawing.Point(267, 486);
+            this.ttlabel.Name = "ttlabel";
+            this.ttlabel.Size = new System.Drawing.Size(180, 45);
+            this.ttlabel.TabIndex = 42;
+            // 
+            // receiptbackgroundLabel
+            // 
+            this.receiptbackgroundLabel.BackColor = System.Drawing.Color.White;
+            this.receiptbackgroundLabel.Location = new System.Drawing.Point(499, 139);
+            this.receiptbackgroundLabel.Name = "receiptbackgroundLabel";
+            this.receiptbackgroundLabel.Size = new System.Drawing.Size(490, 542);
+            this.receiptbackgroundLabel.TabIndex = 17;
+            // 
+            // chLabel
+            // 
+            this.chLabel.BackColor = System.Drawing.Color.Transparent;
+            this.chLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chLabel.Location = new System.Drawing.Point(267, 696);
+            this.chLabel.Name = "chLabel";
+            this.chLabel.Size = new System.Drawing.Size(180, 45);
+            this.chLabel.TabIndex = 43;
+            // 
+            // coverLabel
+            // 
+            this.coverLabel.BackColor = System.Drawing.Color.White;
+            this.coverLabel.Location = new System.Drawing.Point(499, 139);
+            this.coverLabel.Name = "coverLabel";
+            this.coverLabel.Size = new System.Drawing.Size(490, 542);
+            this.coverLabel.TabIndex = 44;
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.BackColor = System.Drawing.Color.Transparent;
+            this.warningLabel.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.warningLabel.Location = new System.Drawing.Point(52, 96);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(395, 45);
+            this.warningLabel.TabIndex = 45;
+            // 
+            // background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.BackgroundImage = global::SecondSummative.Properties.Resources.cute;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1110, 881);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.coverLabel);
+            this.Controls.Add(this.chLabel);
+            this.Controls.Add(this.ttlabel);
+            this.Controls.Add(this.txLabel);
+            this.Controls.Add(this.stLabel);
+            this.Controls.Add(this.neworderButton);
+            this.Controls.Add(this.receiptbackgroundLabel);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.dividerLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.itemsButton);
+            this.Controls.Add(this.treatsInput);
+            this.Controls.Add(this.toysInput);
+            this.Controls.Add(this.collarInput);
+            this.Controls.Add(this.toyLabel);
+            this.Controls.Add(this.treatLabel);
+            this.Controls.Add(this.collarLabel);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "background";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label collarLabel;
+        private System.Windows.Forms.Label treatLabel;
+        private System.Windows.Forms.Label toyLabel;
+        private System.Windows.Forms.TextBox collarInput;
+        private System.Windows.Forms.TextBox toysInput;
+        private System.Windows.Forms.TextBox treatsInput;
+        private System.Windows.Forms.Button itemsButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label dividerLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Button neworderButton;
+        private System.Windows.Forms.Label stLabel;
+        private System.Windows.Forms.Label txLabel;
+        private System.Windows.Forms.Label ttlabel;
+        private System.Windows.Forms.Label receiptbackgroundLabel;
+        private System.Windows.Forms.Label chLabel;
+        private System.Windows.Forms.Label coverLabel;
+        private System.Windows.Forms.Label warningLabel;
+    }
+}
+
---
+++ SecondSummative/Form1.cs
@@ -0,0 +1,202 @@
+﻿///Astoria Buzek
+///Pet Shop
+///March 2018
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace SecondSummative
+{
+    public partial class background : Form
+    {
+        //Variable Declaration
+
+        double subtotal;
+        double tax;
+        double total;
+        double tendered;
+        double change;
+        double collarAmount;
+        double treatAmount;
+        double toyAmount;
+        const double TAX_PERCENT = 0.13;
+        const double COLLAR_COST = 6.5;
+        const double TREAT_COST = 4.75;
+        const double TOY_COST = 1.5;
+        double collarTotal;
+        double treatTotal;
+        double toyTotal;
+
+        SoundPlayer player = new SoundPlayer(Properties.Resources.CatMeow);
+
+     
+
+
+        public background()
+        {
+            InitializeComponent();
+
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            //Play Noise
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Printer);
+            player.Play();
+
+            //Receipt Visability
+            coverLabel.Visible = false;
+
+            //Enter Drawing Tools
+            Graphics g = receiptbackgroundLabel.CreateGraphics();
+            Font drawFont = new Font("Lucidia Console", 8, FontStyle.Regular);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            Refresh();
+
+            //print reciept
+            g.DrawString("Astoria's Pet Shop", drawFont, drawBrush, 105, 1);
+            g.DrawString("Order Number 2733", drawFont, drawBrush, 1, 25);
+            g.DrawString("March 1st, 2018", drawFont, drawBrush, 1, 45);
+
+            g.DrawString("Collars", drawFont, drawBrush, 1, 80);
+            g.DrawString(collarInput.Text, drawFont, drawBrush, 150, 80);
+            g.DrawString("x", drawFont, drawBrush, 215, 80);
+            g.DrawString(COLLAR_COST.ToString("C"), drawFont, drawBrush, 235, 80);
+            g.DrawString("=", drawFont, drawBrush, 300, 80);
+            g.DrawString(collarTotal.ToString("C"), drawFont, drawBrush, 320, 80);
+
+            g.DrawString("Treats", drawFont, drawBrush, 1, 120);
+            g.DrawString(treatsInput.Text, drawFont, drawBrush, 150, 120);
+            g.DrawString("x", drawFont, drawBrush, 215, 120);
+            g.DrawString(TREAT_COST.ToString("C"), drawFont, drawBrush, 235, 120);
+            g.DrawString("=", drawFont, drawBrush, 300, 120);
+            g.DrawString(treatTotal.ToString("C"), drawFont, drawBrush, 320, 120);
+
+            g.DrawString("Toys", drawFont, drawBrush, 1, 160);
+            g.DrawString(toysInput.Text, drawFont, drawBrush, 150, 160);
+            g.DrawString("x", drawFont, drawBrush, 215, 160);
+            g.DrawString(TOY_COST.ToString("C"), drawFont, drawBrush, 235, 160);
+            g.DrawString("=", drawFont, drawBrush, 300, 160);
+            g.DrawString(toyTotal.ToString("C"), drawFont, drawBrush, 320, 160);
+
+            g.DrawString("Subtotal", drawFont, drawBrush, 1, 220);
+            g.DrawString(subtotal.ToString("C"), drawFont, drawBrush, 320, 220);
+
+            g.DrawString("Tax", drawFont, drawBrush, 1, 260);
+            g.DrawString(tax.ToString("C"), drawFont, drawBrush, 320, 260);
+
+            g.DrawString("Total", drawFont, drawBrush, 1, 300);
+            g.DrawString(total.ToString("C"), drawFont, drawBrush, 320, 300);
+
+            g.DrawString("Tendered", drawFont, drawBrush, 1, 360);
+            g.DrawString(tendered.ToString("C"), drawFont, drawBrush, 320, 360);
+
+            g.DrawString("Change", drawFont, drawBrush, 1, 400);
+            g.DrawString(change.ToString("C"), drawFont, drawBrush, 320, 400);
+
+            g.DrawString("Have a nice day!", drawFont, drawBrush, 105, 470);
+            g.DrawString("Telephone (123) 456-7890", drawFont, drawBrush, 70, 500);
+
+
+        }
+
+
+        private void itemsButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Play Noise
+                player.Play();
+
+                //Variable Assignment
+
+                collarAmount = Convert.ToInt16(collarInput.Text);
+                treatAmount = Convert.ToInt16(treatsInput.Text);
+                toyAmount = Convert.ToInt16(toysInput.Text);
+
+                subtotal = collarAmount * COLLAR_COST + treatAmount * TREAT_COST + toyAmount * TOY_COST;
+                tax = subtotal * TAX_PERCENT;
+                total = subtotal + tax;
+                collarTotal = collarAmount * COLLAR_COST;
+                treatTotal = treatAmount * TREAT_COST;
+                toyTotal = toyAmount * TOY_COST;
+
+                stLabel.Text = subtotal.ToString("C");
+                txLabel.Text = tax.ToString("C");
+                ttlabel.Text = total.ToString("C");
+            }
+
+            catch
+
+            {
+                //Mistake
+                warningLabel.Text = "Inputs Must be Numeric!!";
+            }
+
+
+            
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Play Noise
+                player.Play();
+
+                //Tendered and Change 
+                tendered = Convert.ToDouble(tenderedInput.Text);
+                change = tendered - total;
+                chLabel.Text = change.ToString("C");
+            }
+            catch
+            {
+                //Mistake
+                warningLabel.Text = "Inputs Must be Numeric!!";
+            }
+        }
+
+        private void neworderButton_Click(object sender, EventArgs e)
+        {
+            //Play Noise
+            player.Play();
+
+            //Clear Screen
+            collarInput.Clear();
+            treatsInput.Clear();
+            toysInput.Clear();
+            tenderedInput.Clear();
+
+            stLabel.Text = "";
+            txLabel.Text = "";
+            ttlabel.Text = "";
+            chLabel.Text = "";
+
+            subtotal = 0;
+            tax = 0;
+            total = 0;
+            tendered = 0;
+            change = 0;
+            collarAmount = 0;
+            treatAmount = 0;
+            toyAmount = 0;
+            collarTotal = 0;
+            treatTotal = 0;
+            toyTotal = 0;
+
+            coverLabel.Visible = true;
+
+        }
+    }
+}
